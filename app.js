08645b30c4a394111c9ad333b027fb6328e92b7b
Matthew <mattsully01@gmail.com>
2019-11-26 14:13:07 -0700

    deploying task list to heroku

--- app.js
+++ app.js
@@ -10,40 +10,66 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 // load all event listeners 
-function loadEventListeners(){
+function loadEventListeners() {
     // add event task
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click',removeTask);
+    // clear all tasks
+    claerBtn.addEventListener('click', clearTasks);
 }
 
 // add task
-function addTask(e){
+function addTask(e) {
     e.preventDefault();
 
 
-    if(taskInput.value === ''){
+    if (taskInput.value === '') {
         alert('Add a task.')
+    }else{
+
+        //Create li element
+        const li = document.createElement('li');
+        // add class
+        li.className = 'collection-item';
+        // create text node and append to li 
+        li.appendChild(document.createTextNode(taskInput.value));
+        // Create new link element 
+        const link = document.createElement('a');
+        // add class
+        link.className = 'delete-item secondary-content';
+        //add iconHTML 
+        link.innerHTML = '<i class="fas fa-minus-circle fa-lg red-text "></i>';
+        // append the link to li
+        li.appendChild(link); 
+        // append li to ul 
+        taskList.appendChild(li);
     }
-    
-    //Create li element
-    const li = document.createElement('li');
-    // add class
-    li.className = 'collection-item';
-    // create text node and append to li 
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new link element 
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    //add iconHTML 
-    link.innerHTML = '<i class="fas fa-minus-circle fa-lg red-text "></i>';
-    // append the link to li
-    li.appendChild(link);
-    // append li to ul 
-    taskList.appendChild(li);
+
 
     // clear input 
-    taskInput.value='';
+    taskInput.value = '';
 
 }
+
+// remove task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+         if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+
+         }
+    }
+}
+
+// clear tasks 
+function clearTasks(e){
+     while(taskList.firstChild){
+       taskList.removeChild(taskList.firstChild);  
+     };
+}
+
+
 
 
 
