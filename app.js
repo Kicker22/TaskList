05bbaa9308f909fe6fd29028c8541d7d710c9d97
Matthew <mattsully01@gmail.com>
2019-11-26 15:38:08 -0700

    fixing some bugs with local storage

--- app.js
+++ app.js
@@ -89,9 +89,9 @@ function addTask(e) {
 
     }
 
-    
-    
-    
+
+
+
 }
 
 // Store Task
@@ -114,8 +114,30 @@ function removeTask(e) {
         if (confirm('Are you sure?')) {
             e.target.parentElement.parentElement.remove();
 
+            // remove from LS
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+
+// remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function (task, index) {
+        if (taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // clear tasks 
